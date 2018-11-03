8e024899f07aab13abea8f18042fa83b7b3d1493
liangdalong <liangdalong2010@gmail.com>
2018-11-03 14:07:50 +0800

    mat-5

---
+++ mat-temp5.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import cv2 as cv
+import numpy as np
+
+
+def template_image():
+    target = cv.imread('image/990436704.jpg')
+    tpl = cv.imread('image/t3.jpg')
+    # cv.imshow("modul", tpl)
+    # cv.imshow("yuan", target)
+    methods = [cv.TM_SQDIFF_NORMED, cv.TM_CCORR_NORMED, cv.TM_CCOEFF_NORMED]
+    th, tw = tpl.shape[:2]
+    for md in methods:
+        result = cv.matchTemplate(target, tpl, md)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+        if md == cv.TM_SQDIFF_NORMED:
+            tl = min_loc
+        else:
+            tl = max_loc
+        br = (tl[0] + tw, tl[1] + th)
+        cv.rectangle(target, tl, br, [0, 0, 0])
+        cv.imshow("pipei" + np.str(md), target)
+
+
+template_image()
+cv.waitKey(0)
+cv.destroyAllWindows()
